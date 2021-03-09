2082c7128ececc20a4c934c290941c105e6bb2db
vvnwu <wu.vivian00@gmail.com>
2021-03-09 01:46:29 -0800

    updated main ble transmission with new charac

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const bleno = require('bleno');
+const BlenoPrimaryService = bleno.PrimaryService;
+const TokenCharacteristic = require('./characteristics/auth/tokenCharacteristic');
+const VoteCharacteristic = require('./characteristics/voteCharacteristic');
+
+const User = require('./models/user');
+let user = new User();
+
+console.log('bleno -pi server');
+bleno.on('stateChange', function(state){
+	console.log('on -> stateChange: ' + state);
+
+	if(state === 'poweredOn'){
+		bleno.startAdvertising('Voting-Pi', ['vp00']);
+	}else{
+		bleno.stopAdvertising();
+	}
+});
+bleno.on('advertisingStart', function(error){
+	console.log('on -> advertisingStart: ' + (error? 'error' + error : 'success'));
+	if(!error){
+		bleno.setServices([
+			new BlenoPrimaryService({
+				uuid: 'vp00',
+				characteristics: [
+					new TokenCharacteristic(user),
+					new VoteCharacteristic(user)
+				]
+			})
+		]);
+	}
+});
