757ac91bc96f4abd1da2c2dad765f80297419561
sherman-l <slok6@my.bcit.ca>
2021-03-30 06:00:00 +0900

    replacing previous commit, committed wrong file

--- characteristics/tokenCharacteristic.js
+++ characteristics/tokenCharacteristic.js
@@ -6,25 +6,36 @@ const BlenoDescriptor = bleno.Descriptor;
 
 const { resolve } = require('path');
 const apiUrl = 'https://good-team.herokuapp.com';
-var user;
 
-const getElectionId = async () => {
+
+const getElectionId = async (user) => {
 	try {
-		console.log('requesting from\n'  + apiUrl + '/eligible/' + user);
-		return await axios.get(apiUrl+'/eligible/'+user);
+		console.log('requesting from\n'  + apiUrl + '/eligible/' + user.id);
+		return await axios.get(apiUrl+'/eligible/'+user.id);
 	} catch (error) {
 		console.error(error);
 	}
 }
 
-const getElectionData = async (electionId) => {
+const getElectionData = async (electionId, token) => {
 	try {
-		return await axios.get(apiUrl+'/elections/'+electionId);
+		return await axios.get(apiUrl+'/elections/'+electionId, {headers: {
+      "voter-token": token
+    }});
 	} catch(error) {
 		console.error(error);
 	}
 };
 
+const getToken = async (email, pass) => {
+  try {
+    console.log("grabbing token from url");
+    let payload = {"email": email, "password": pass};
+    return await axios.post(apiUrl+'/login', payload);
+  } catch (error) {
+      console.error(error);
+  }  
+}
 
 function TokenCharacteristic(user){
     TokenCharacteristic.super_.call(this, {
@@ -40,50 +51,77 @@ function TokenCharacteristic(user){
     this._updateValueCallback = null;
     this._token = new Buffer(0);
     this._MTU = 20;
+    this.user = user;
 }
 
 util.inherits(TokenCharacteristic, BlenoCharacteristic);
 
 TokenCharacteristic.prototype.onWriteRequest = async function(data, offset, withoutResponse, callback) {
-    this._token = data;
-    user = data;
-    console.log(`TokenCharacteristic - onWriteRequest: value = ${this._token}`);
+  data = JSON.parse(data);
+  console.log(data.email);
+  console.log(data.password);
 
-    if(this._updateValueCallback){
-      let MTU = this._MTU;
-      //authenticate user and get their elections here
-
-      let dataPromise = new Promise(async (resolve, reject) => {
-        let eligibleElectionIds = await getElectionId();
-        eligibleElectionIds = eligibleElectionIds.data.voteless;
-        let electionData = [];
-        let queryPromise = new Promise(async (resolve, reject) => {
-          for(const eligibleId of eligibleElectionIds) {
-            const result = await getElectionData(eligibleId);
-            electionData.push(result.data);
+  
+  let tokenPromise = new Promise(async (resolve, reject) => {
+    console.log("within token promise");
+    let loginResponse = await getToken(data.email, data.password);
+    if(!loginResponse) {
+      reject();
+    }
+    resolve(loginResponse);
+  });
+  tokenPromise.then(
+    async (datastream) => {
+      console.log("inside tokenpromise: " + datastream.data.token);
+      this._token = datastream.data.token;
+      this.user.id = datastream.data._id;
+      if(this._updateValueCallback){
+        let MTU = this._MTU;
+        //authenticate user and get their elections here
+  
+        let dataPromise = new Promise(async (resolve, reject) => {
+          console.log("from update callback promise: " + this._token);
+          let eligibleData = await axios.get(apiUrl + "/voters/elections", {headers: {
+            "voter-token": this._token
+          }});
+          let eligibleElectionIds = eligibleData.data.votable;
+          let electionData = [];
+          let queryPromise = new Promise(async (resolve, reject) => {
+            for(const eligibleId of eligibleElectionIds) {
+              const result = await getElectionData(eligibleId, this._token);
+  
+              electionData.push(result.data.election);
+            }
+          resolve();
+          });
+          queryPromise.then(() => {
+            resolve(electionData);
+          });
+        });
+        dataPromise.then((datastream) => {
+          console.log(`TokenCharacteristic - onWriteRequest: notifying`);
+          datastream = JSON.stringify(datastream);
+          //sends chunked elections payload to mobile client
+          for(let i = 0; i<datastream.length/MTU;i++){
+            let buff = new Buffer(datastream.substr(i*MTU, MTU));
+            console.log('Notifying: '+buff.toString());
+            this._updateValueCallback(buff);
           }
-        resolve();
+          this._updateValueCallback(new Buffer([0x00]));
+          callback(this.RESULT_SUCCESS);  
         });
-        queryPromise.then(() => {
-          resolve(electionData);
-        });
-      });
-      dataPromise.then((datastream) => {
-        console.log(`TokenCharacteristic - onWriteRequest: notifying`);
-        datastream = JSON.stringify(datastream);
-        //sends chunked elections payload to mobile client
-        for(let i = 0; i<datastream.length/MTU;i++){
-          let buff = new Buffer(datastream.substr(i*MTU, MTU));
-          console.log('Notifying: '+buff.toString());
-          this._updateValueCallback(buff);
-        }
-        this._updateValueCallback(new Buffer([0x00]));
-        callback(this.RESULT_SUCCESS);  
-      });
-  }else{
-    console.log(`TokenCharacteristic - onWriteRequest: notifying, no device to notify.`)
-    callback(this.RESULT_UNLIKELY_ERROR);
-  }
+    }else{
+      console.log(`TokenCharacteristic - onWriteRequest: notifying, no device to notify.`)
+      callback(this.RESULT_UNLIKELY_ERROR);
+    }
+  
+    },
+    (err) => console.error(err));
+ 
+  //   this._token = data;
+  //   this.user.id = data;
+  //   console.log(`TokenCharacteristic - onWriteRequest: value = ${this._token}`);
+
 };
 
 TokenCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback){
